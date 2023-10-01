2af65612a71c3eca0b0a974f745b991df872ab7e
SlapClaps <kevinjoh@hotmail.com>
2023-10-01 11:13:04 +0200

    Updated version

---
+++ LibraryNetworking/QtLibGUI/Client.h
@@ -0,0 +1,195 @@
+#pragma once
+
+#include <iostream>
+#include <olc_net.h>
+#include <QObject>
+
+
+enum class CustomMsgTypes : uint32_t
+{
+	ServerAccept,
+	ServerDeny,
+	ServerPing,
+	MessageAll,
+	MessageServer,
+	ServerMessage,
+
+	Login,
+
+	LoanBook,
+	ReturnBook,
+	AddBook,
+	RemoveBook,
+	InformationBook,
+	IsAvailableBook,
+	ListBooks,
+	ListMembers,
+	ListLibrarians,
+	AddMember,
+	RemoveMember,
+	AddLibrarian,
+	RemoveLibrarian,
+};
+
+class CustomClient : public QObject, public olc::net::client_interface<CustomMsgTypes>
+{
+	Q_OBJECT
+
+public:
+	olc::net::tsqueue<std::string> inputQueue;
+	bool isLoggedIn = false;
+	bool loginApproval = false;  // brukes ikke enda
+
+
+	void PingServer()
+	{
+		olc::net::message<CustomMsgTypes> msg;
+		msg.header.id = CustomMsgTypes::ServerPing;
+
+		// Caution with this...
+		std::chrono::system_clock::time_point timeNow = std::chrono::system_clock::now();
+
+		msg << timeNow;
+		Send(msg);
+	}
+
+	void MessageAll()
+	{
+		olc::net::message<CustomMsgTypes> msg;
+		msg.header.id = CustomMsgTypes::MessageAll;
+		Send(msg);
+	}
+
+	void MessageServer(const std::string& stringMsg)
+	{
+		olc::net::message<CustomMsgTypes> msg;
+		msg.header.id = CustomMsgTypes::MessageServer;
+		std::vector<char> vec(stringMsg.begin(), stringMsg.end());
+		for (int i = 0; i < vec.size(); ++i)
+		{
+			msg << vec[i];
+		}
+		Send(msg);
+	}
+
+	void addBookCommand(std::string& stringMsg)
+	{
+		olc::net::message<CustomMsgTypes> msg;
+		msg.header.id = CustomMsgTypes::AddBook;
+		std::vector<char> vec(stringMsg.begin(), stringMsg.end());
+		for (int i = 0; i < vec.size(); ++i)
+		{
+			msg << vec[i];
+		}
+		Send(msg);
+	}
+
+	void addMemberCommand(std::string& stringMsg)
+	{
+		olc::net::message<CustomMsgTypes> msg;
+		msg.header.id = CustomMsgTypes::AddMember;
+		std::vector<char> vec(stringMsg.begin(), stringMsg.end());
+		for (int i = 0; i < vec.size(); ++i)
+		{
+			msg << vec[i];
+		}
+		Send(msg);
+	}
+
+	void listBooks()
+	{
+		olc::net::message<CustomMsgTypes> msg;
+		msg.header.id = CustomMsgTypes::ListBooks;
+		Send(msg);
+	}
+
+	void listMembers()
+	{
+		olc::net::message<CustomMsgTypes> msg;
+		msg.header.id = CustomMsgTypes::ListMembers;
+		Send(msg);
+	}
+
+	void readInput()
+	{
+		std::string input;
+		while (true)
+		{
+			std::getline(std::cin, input);
+			inputQueue.push_back(input);
+		}
+	}
+
+	// Sends username and password in a format like this: username;password;.
+	void handleLogin(std::string& inputUsername, std::string& inputPassword)
+	{
+		olc::net::message<CustomMsgTypes> msg;
+		msg.header.id = CustomMsgTypes::Login;
+		std::vector<char> uvec(inputUsername.begin(), inputUsername.end());
+		for (int i = 0; i < uvec.size(); ++i)
+		{
+			msg << uvec[i];
+		}
+		msg << ';';
+		std::vector<char> pvec(inputPassword.begin(), inputPassword.end());
+		for (int i = 0; i < pvec.size(); ++i)
+		{
+			msg << pvec[i];
+		}
+		msg << ';';
+		Send(msg);
+	}
+
+	// For the login response
+	std::string waitForResponse(CustomClient& c)
+	{
+		std::string output;
+		int i = 0;
+		while (i < 5)
+		{
+			while (!c.Incoming().empty())
+			{
+				auto msg = c.Incoming().pop_front().msg;  // "Moves" the msg from the queue into a new msg variable to use here.
+
+				if (msg.header.id == CustomMsgTypes::Login)  // Check what type of msg was received.
+				{
+					// Server has responded to a login request	
+					char ch;
+					while (msg.body.size() > 0)
+					{
+						msg >> ch;
+						output = ch + output;  // Prepend the character to the string
+					}
+					std::cout << "[Server message]: " << output << "\n";
+					if (output == "Approval")  // If server sends an approval to client.
+					{
+						c.isLoggedIn = true;
+						i = 5;
+					}
+				}
+				if (c.Incoming().empty()) {
+					return output;
+				}
+			}
+			++i;
+			std::this_thread::sleep_for(std::chrono::seconds(1));
+		}
+		return output;
+	}
+
+	// For any message, but doesn't check what kind of message.
+	std::string getMessage(CustomClient& c) {
+		std::string response;
+		if (!c.Incoming().empty()) {
+			auto msg = c.Incoming().pop_front().msg;
+			char ch;
+			while (msg.body.size() > 0)
+			{
+				msg >> ch;
+				response = ch + response;  // Prepend the character to the string
+			}
+		}
+		return response;
+	}
+
+};
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # LibraryApplication
+
+Right-click on the NetServer project and run debug. Server will be running and able to connect to clients.
+Right-click on the QtLibGUI project and run debug. One client will connect and client application will run. You can have multiple clients open simultaneously.
